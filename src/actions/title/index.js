4092ed44a86c718cab4141996e1d3573bb3e9677
901david <vdavidhammond@gmail.com>
2018-06-12 12:54:02 -0600

    Implemented basic redux setup

---
+++ src/actions/title/index.js
@@ -0,0 +1,9 @@
+import { ADD_TITLE } from '../../types';
+
+
+export const changeTitle = (title) => {
+  return {
+    type: ADD_TITLE,
+    payload: title,
+  }
+}
--- src/components/App.js
+++ src/components/App.js
@@ -1,13 +1,47 @@
 import React, { Component } from 'react';
+import { connect } from 'react-redux';
+import { changeTitle } from '../actions/title';
 
 class App extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      text: ''
+    }
+    this.renderNewTitle = this.renderNewTitle.bind(this);
+    this.onInputChange = this.onInputChange.bind(this);
+  }
+
+  renderNewTitle() {
+    this.props.changeTitle(this.state.text);
+  }
+
+  onInputChange(event) {
+    this.setState({ text: event.target.value});
+  }
+
   render() {
+    let input;
     return (
       <div>
-        <h1>Welcome to React Boilerplate</h1>
+        <h1>{this.props.title}</h1>
+        <input onChange={this.onInputChange} type="text" value={this.state.text} />
+        <div onClick={this.renderNewTitle} className='btn btn-primary'>
+          Add a new title
+        </div>
       </div>
     );
   }
 }
 
-export default App;
+const mapStateToProps = (state) => ({
+  title: state.title.title
+});
+
+const mapDispatchToProps = (dispatch) => ({
+  changeTitle: title => dispatch(changeTitle(title))
+});
+
+
+
+export default connect(mapStateToProps, mapDispatchToProps)(App);
