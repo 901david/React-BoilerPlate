aa3a5a2f5e672b1ac3d4d0cf819d1e6d914dd567
901david <vdavidhammond@gmail.com>
2018-06-12 17:53:03 -0600

    Added in Redux utils

--- src/index.js
+++ src/index.js
@@ -19,7 +19,7 @@ const store = createStore(
 ReactDOM.render(
   <Provider store={store}>
     <Router>
-      <Route path="/:filter?" component={App} />
+      <Route path="/" component={App} />
     </Router>
   </Provider>,
   document.getElementById("app")
